3a715406d1fe0f8d5479fe5938da81cdd7f3720f
Ryan Gabryel <85703424+ryserk@users.noreply.github.com>
2023-08-17 10:54:38 -0300

    Delete códigos/bazinga directory

--- códigos/bazinga/target/debug/bazinga.d
+++
@@ -1,1 +0,0 @@
-/home/pc123/codigos/rust/aprendendo/bazinga/target/debug/bazinga: /home/pc123/codigos/rust/aprendendo/bazinga/src/main.rs
--- códigos/bazinga/target/debug/deps/bazinga-b5afcfabc11c7171.d
+++
@@ -1,5 +0,0 @@
-/home/pc123/codigos/rust/aprendendo/bazinga/target/debug/deps/bazinga-b5afcfabc11c7171.rmeta: src/main.rs
-
-/home/pc123/codigos/rust/aprendendo/bazinga/target/debug/deps/bazinga-b5afcfabc11c7171.d: src/main.rs
-
-src/main.rs:
