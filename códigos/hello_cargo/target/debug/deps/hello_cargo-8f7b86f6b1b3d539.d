b70396539ddc01d734572eea221a8f48e117814c
Ryan Gabryel <85703424+ryserk@users.noreply.github.com>
2023-08-17 10:55:28 -0300

    Delete códigos/hello_cargo directory

--- códigos/hello_cargo/target/debug/deps/hello_cargo-8f7b86f6b1b3d539.d
+++
@@ -1,5 +0,0 @@
-/home/pc123/codigos/rust/aprendendo/hello_cargo/target/debug/deps/hello_cargo-8f7b86f6b1b3d539.rmeta: src/main.rs
-
-/home/pc123/codigos/rust/aprendendo/hello_cargo/target/debug/deps/hello_cargo-8f7b86f6b1b3d539.d: src/main.rs
-
-src/main.rs:
--- códigos/hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/home/pc123/codigos/rust/aprendendo/hello_cargo/target/debug/hello_cargo: /home/pc123/codigos/rust/aprendendo/hello_cargo/src/main.rs
